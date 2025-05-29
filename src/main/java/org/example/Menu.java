bcd19e615566ca63137db805e1de8af596b0e0cb
marvin <marvinjonasjeuk@gmail.com>
2025-05-29 21:32:41 +0200

    SpielerObjekt hat jetzt ein bool für Bot Menüführung mit spieleranzahl zur auswahl wenn 2 Spieler ausgewählt werden wird in zukunft 4-2 gerechnet und somit 2 bots ausgewählt mit der ausgewählten schwierigkeit

---
+++ src/main/java/org/example/Menu.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Menu {
+
+    public int Menu(int spieleranzahl) throws Exception {
+
+        System.out.println("How many players want to play? max. 4 players");
+
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        spieleranzahl = Integer.parseInt(read.readLine());
+
+        try{
+            if(spieleranzahl<1 || spieleranzahl>4) { //kein Spielabbruch bei falscher Zahleingabe
+                return Menu(spieleranzahl);
+            }
+        } catch (IOException e){
+            return Menu(spieleranzahl);
+        }
+
+        Runde runde = new Runde(spieleranzahl);
+        runde.start();
+        return spieleranzahl;
+    }
+}
